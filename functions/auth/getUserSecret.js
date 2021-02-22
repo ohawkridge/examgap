ea3bf5cccd243432eb5666981e0be1158b421439
Owen Hawkridge <ohawkridge@gmail.com>
2021-02-22 12:38:35 +0000

    Update getUserSecret

--- functions/auth/getUserSecret.js
+++ functions/auth/getUserSecret.js
@@ -1,9 +1,9 @@
-var faunadb = require("faunadb"),
-  q = faunadb.query;
+const faunadb = require('faunadb')
+const q = faunadb.query
 
 // configure fauna client with login secret
 const client = new faunadb.Client({
-  secret: process.env.SECRET_KEY
+  secret: process.env.SECRET_KEY,
 })
 
 // Try for a secret token with user's credentials
@@ -13,20 +13,17 @@ exports.handler = async (event, context, callback) => {
   console.log(`credentials...`, data)
   // Use login key to try credentials
   try {
-    // const { secret } = await client.query(
-    //   q.Login(q.Match(q.Index("user_by_username"), username), {
-    //     password,
-    //     ttl: q.TimeAdd(q.Now(), 7, "days"),
-    //   })
-    // );
+    const { secret } = await client.query(
+      q.Login(q.Match(q.Index('user_by_username'), data.username), {
+        password: data.password,
+        ttl: q.TimeAdd(q.Now(), 7, 'days'),
+      })
+    )
     return {
       statusCode: 200,
-      body: JSON.stringify('your token'),
-    };
+      body: JSON.stringify({ secret }),
+    }
   } catch (err) {
-    return { statusCode: 500, body: err.toString() };
+    return { statusCode: 500, body: err.toString() }
   }
-}
-
-
 }
