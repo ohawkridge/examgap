6d6dc689ed937c12f02d5e29703137afb12369c2
Owen Hawkridge <ohawkridge@gmail.com>
2021-05-26 20:00:31 +0100

    Loaders instead of async

--- pages/assignment/_assignment.vue
+++ pages/assignment/_assignment.vue
@@ -1,7 +1,29 @@
 <template>
   <v-row class="justify-center">
     <v-col cols="12" lg="10">
-      <v-card class="pa-md-3 mt-md-3">
+      <v-sheet v-if="$fetchState.pending" elevation="2" class="pa-7 mt-md-3">
+        <v-skeleton-loader
+          v-bind="attrs"
+          type="heading"
+          :loading="$fetchState.pending"
+          class=""
+          height="40"
+        ></v-skeleton-loader>
+        <v-skeleton-loader
+          v-bind="attrs"
+          type="text@2"
+          :loading="$fetchState.pending"
+          width="200"
+        ></v-skeleton-loader>
+        <v-skeleton-loader
+          v-bind="attrs"
+          type="image@2"
+          :loading="$fetchState.pending"
+          width="95%"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-sheet>
+      <v-card v-if="!$fetchState.pending" class="pa-md-3 mt-md-3">
         <v-card-text>
           <v-row>
             <v-col>
@@ -39,20 +61,45 @@ export default {
     AssignmentQuestion,
   },
   layout: 'app',
-  async asyncData({ store, params, $config: { baseURL } }) {
-    const url = new URL('/.netlify/functions/getAssignment', baseURL)
+  // async asyncData({ store, params, $config: { baseURL } }) {
+  //   const url = new URL('/.netlify/functions/getAssignment', baseURL)
+  //   const response = await fetch(url, {
+  //     body: JSON.stringify({
+  //       secret: store.state.user.secret,
+  //       assignmentId: params.assignment,
+  //     }),
+  //     method: 'POST',
+  //   })
+  //   if (!response.ok) {
+  //     throw new Error(`Error fetching assignment ${response.status}`)
+  //   }
+  //   const assignment = await response.json()
+  //   return { assignment }
+  // },
+  data() {
+    return {
+      assignment: {},
+      attrs: {
+        class: 'mb-6',
+      },
+    }
+  },
+  async fetch() {
+    const url = new URL(
+      '/.netlify/functions/getAssignment',
+      this.$config.baseURL
+    )
     const response = await fetch(url, {
       body: JSON.stringify({
-        secret: store.state.user.secret,
-        assignmentId: params.assignment,
+        secret: this.$store.state.user.secret,
+        assignmentId: this.$route.params.assignment,
       }),
       method: 'POST',
     })
     if (!response.ok) {
       throw new Error(`Error fetching assignment ${response.status}`)
     }
-    const assignment = await response.json()
-    return { assignment }
+    this.assignment = await response.json()
   },
   created() {
     this.$icons = { mdiArrowLeft }
