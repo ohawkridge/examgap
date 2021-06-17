4f921e59d95c45859327b4061077c50e927e060e
Owen Hawkridge <ohawkridge@gmail.com>
2021-06-17 18:09:09 +0100

    Remove get-user middleware

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-generate#exclude
   generate: {
     exclude: [
-      /^\/home/,
+      // /^\/home/,
       /^\/classes/,
       /^\/assignment/,
       /^\/course/,
