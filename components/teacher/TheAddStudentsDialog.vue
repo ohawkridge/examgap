385481db75ea4f43913df62f5ae2a6c201ef62a5
Owen Hawkridge <ohawkridge@gmail.com>
2021-11-13 14:43:25 +0000

    Improve add students Andy

--- components/teacher/TheAddStudentsDialog.vue
+++ components/teacher/TheAddStudentsDialog.vue
@@ -7,20 +7,33 @@
           v-model="usernames"
           outlined
           clearable
-          placeholder="One address per line"
+          placeholder="One email per line"
           label="Email addresses*"
           autofocus
         ></v-textarea>
         <v-text-field
           v-model="domain"
-          label="Append email domain"
-          placeholder="yourschool.org.uk"
+          label="Append domain"
+          placeholder="@school.org.uk"
           outlined
+          :rules="[domainRules]"
+          @input="append()"
         >
           <template #append>
-            <v-btn class="fix-btn" text rounded @click="append()">
-              Append
-            </v-btn>
+            <v-menu offset-y open-on-hover>
+              <template #activator="{ on }">
+                <font-awesome-icon
+                  icon="fa-light fa-circle-info"
+                  class="fa-lg"
+                  v-on="on"
+                />
+              </template>
+              <v-card max-width="260">
+                <v-card-text class="text-body-2">
+                  Enter your school's email domain to append to all students.
+                </v-card-text>
+              </v-card>
+            </v-menu>
           </template>
         </v-text-field>
         <div class="d-flex justify-end">
@@ -31,7 +44,7 @@
             class="ml-2"
             rounded
             :loading="loading"
-            :disabled="loading"
+            :disabled="loading || userNamesArray.length === 0"
             @click="addStudents()"
             ><span class="heading--text">Add Students</span></v-btn
           >
@@ -50,14 +63,25 @@ export default {
       loading: false,
       usernames: '',
       domain: '',
+      domainRules: (v) => !!(v[0] === '@') || "'@' sign is required.",
     }
   },
   computed: {
     userNamesArray() {
+      if (this.usernames === null) {
+        return []
+      }
       return this.usernames
         .split(/[\r\n|,]+/)
         .map((name) => name.trim().replace(',', '').toLowerCase())
         .filter((name) => name !== '')
+    },
+  },
+  watch: {
+    dialog() {
+      if (!this.dialog) {
+        this.domain = ''
+      }
     },
   },
   beforeDestroy() {
@@ -70,36 +94,41 @@ export default {
   },
   methods: {
     append() {
-      let out = ''
-      for (let name of this.userNamesArray) {
-        // Remove any old appendings
-        if (name.includes('@')) {
-          name = name.substring(0, name.indexOf('@'))
+      if (this.domain[0] === '@') {
+        let out = ''
+        for (let name of this.userNamesArray) {
+          // Remove any old appendings
+          if (name.includes('@')) {
+            name = name.substring(0, name.indexOf('@'))
+          }
+          out = out + name + this.domain + '\n'
         }
-        out = out + name + '@' + this.domain + '\n'
+        this.usernames = out
       }
-      this.usernames = out
     },
     // N.B. When you add students like this, they *don't*
     // automatically get all existing assignments added
     async addStudents() {
-      try {
-        this.loading = true
-        await this.$store.dispatch('group/addStudents', this.userNamesArray)
-        this.$snack.showMessage({
-          type: 'success',
-          msg: `Student${this.userNamesArray.length !== 1 ? 's' : ''} added`,
-        })
-      } catch (err) {
-        console.error(err)
-        this.$snack.showMessage({
-          type: 'error',
-          msg: 'Error adding students',
-        })
-      } finally {
-        this.dialog = false
-        this.loading = false
-        this.usernames = ''
+      if (this.userNamesArray.length > 0) {
+        try {
+          this.loading = true
+          await this.$store.dispatch('group/addStudents', this.userNamesArray)
+          this.$snack.showMessage({
+            type: 'success',
+            msg: `Student${this.userNamesArray.length !== 1 ? 's' : ''} added`,
+          })
+        } catch (err) {
+          console.error(err)
+          this.$snack.showMessage({
+            type: 'error',
+            msg: 'Error adding students',
+          })
+        } finally {
+          this.dialog = false
+          this.loading = false
+          this.usernames = ''
+          this.domain = ''
+        }
       }
     },
   },
