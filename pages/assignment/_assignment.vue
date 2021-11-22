d8124338649627fbb5292f1976e9f0406b0b6d4f
Owen Hawkridge <ohawkridge@gmail.com>
2021-11-22 13:15:28 +0000

    Mobile design

--- pages/assignment/_assignment.vue
+++ pages/assignment/_assignment.vue
@@ -24,47 +24,70 @@
       </div>
     </template>
     <template v-else>
-      <div class="px-4 py-8 d-flex justify-space-between">
-        <div style="width: 60%">
-          <div class="text-h6">
+      <v-container>
+        <v-row>
+          <v-col cols="12" class="d-flex align-center">
             <v-tooltip bottom>
               <template #activator="{ on }">
-                <v-btn nuxt to="/home" icon class="mr-2" v-on="on">
+                <v-btn icon class="mr-2" @click="$router.go(-1)" v-on="on">
                   <font-awesome-icon
                     icon="fa-light fa-arrow-left"
-                    class="fa-lg ico-btn"
+                    class="ico-btn"
                   />
                 </v-btn>
               </template>
               <span>Back</span>
             </v-tooltip>
-            {{ assignment.name }}
-          </div>
-          <p class="ml-12">
-            {{ assignment.questions.length }} Question{{
-              assignment.questions.length | pluralize
-            }}
-          </p>
-        </div>
-        <div style="width: 40%">
-          <div class="d-flex align-center mb-2">
-            <span class="fix-width font-weight-medium">Start:</span>
-            {{ assignment.start | date }}
-          </div>
-          <div class="d-flex align-center mb-2">
-            <span class="fix-width font-weight-medium">Due:</span>
-            {{ assignment.dateDue | date }}
-          </div>
-          <div class="d-flex align-center">
-            <span class="fix-width font-weight-medium">Score:</span>
-            {{ `${s.total}/${s.max}` }}
-            <v-chip small label :color="s.color" class="ml-2 font-weight-bold">
-              {{ s.ave }}%
-            </v-chip>
-          </div>
-        </div>
-      </div>
-      <v-list style="border-top: 1px solid #d2d2d2 !important" class="py-0">
+            <span class="text-h6">{{ assignment.name }}</span>
+          </v-col>
+          <v-col cols="8">
+            <div class="d-flex align-center mb-2" style="height: 32px">
+              <span class="fix-width font-weight-medium">Start:</span>
+              {{ assignment.start | date }}
+            </div>
+            <div class="d-flex align-center mb-2">
+              <span class="fix-width font-weight-medium">Due:</span>
+              {{ assignment.dateDue | date }}
+            </div>
+          </v-col>
+          <v-col cols="4">
+            <div class="mb-2 d-flex align-center">
+              <span
+                v-if="$vuetify.breakpoint.name !== 'xs'"
+                class="fix-width2 font-weight-medium"
+                >Score:</span
+              >
+              {{ `${s.total}/${s.max}` }}
+              <v-chip label :color="s.color" class="ml-2 font-weight-bold">
+                {{ s.ave }}%
+              </v-chip>
+            </div>
+            <div>
+              <span
+                v-if="$vuetify.breakpoint.name !== 'xs'"
+                class="fix-width2 font-weight-medium"
+                >Accuracy:
+              </span>
+              <font-awesome-icon
+                v-else
+                icon="fa-light fa-bullseye-arrow"
+                class="fa-lg mr-2"
+              />
+              <!-- XX% -->
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <p class="text-subtitle-1 font-weight-bold">
+              Question{{ assignment.questions.length | pluralize }} ({{
+                assignment.questions.length
+              }})
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-list id="q-list">
         <assignment-question
           v-for="(question, i) in assignment.questions"
           :key="i"
@@ -138,11 +161,21 @@ export default {
 /* Align start/due dates */
 .fix-width {
   display: inline-block;
-  width: 72px;
+  width: 56px;
+}
+
+.fix-width2 {
+  display: inline-block;
+  width: 80px;
 }
 
 .ico-btn {
   height: 24px;
   width: 24px;
+}
+
+#q-list {
+  border-top: 1px solid #d2d2d2 !important;
+  background-color: #fafafa;
 }
 </style>
